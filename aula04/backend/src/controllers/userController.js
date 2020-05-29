c66f1803c7d2a33fc48d509608737863c7b1b1b3
Adriel Almeida <adrielalmeidap@gmail.com>
2020-05-29 19:33:13 -0300

    alteracoes

--- aula04/backend/src/controllers/userController.js
+++ aula04/backend/src/controllers/userController.js
@@ -7,12 +7,40 @@ module.exports = {
         User.create(response);
         res.json(response);
     },
+
     async listUsers( req, res ) {
         const response = await User.find();
         res.json(response);
     },
+
     async listOne(req, res) {
-        const response = await User.findById("5ed05a8108fe4a1bfc7caf79");
+        const response = await User.findById(req.params.id);
+        res.json(response);
+    },
+
+    async listBySex(req,res){
+        const response = await User.find({"sex" : req.params.sex});
+        
+        //const response = await User.find({age : {$gte:45}},{sex : 'Feminino'});
+        /*Fazendo da forma abaixo, você pode procurar por uma parte do texto
+        da String que queria procurar, fazendo uma pesquisa basicamente como se
+        estivesse usando um like '%%' no SQL
+        const response = await User.find({name : /Bastos/});*/
+        res.json(response);
+    },
+
+    //Criar outro método que traga todos que tenham menos de X anos
+    //Para criar esses métodos, verificar no link da documentação oficial
+    //do Mangoose: https://mongoosejs.com/
+
+    async listByAge(req,res){
+        //const response = await User.where('this.age.indexOf("val" >= 28').exec();
+        const response = await User.find({"age" : {$gt : req.params.age}});
+        res.json(response);
+    },
+    
+    async listManyFields(req, res) {
+        const response = await req.query.fields;
         res.json(response);
     }
 }
--- aula04/backend/src/routes.js
+++ aula04/backend/src/routes.js
@@ -4,6 +4,9 @@ const userController = require('./controllers/userController');
 
 routes.post('/user',userController.persistData);
 routes.get('/user',userController.listUsers);
-routes.get('/userOne',userController.listOne);
+routes.get('/userOne/:id',userController.listOne);
+routes.get('/user/:sex',userController.listBySex);
+routes.get('/userAge/:age',userController.listByAge);
+routes.get('/userFields/:fields',userController.listManyFields);
 
 module.exports = routes;
